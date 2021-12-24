ebe82e66fbf393100e07339bf2e8a11952f8af7e
Sadaf Rahman <62661608+rahmansadaf46@users.noreply.github.com>
2021-12-24 15:16:44 +0600

    all api complete

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const ObjectId = require('mongodb').ObjectId;
 const fileUpload = require('express-fileupload');
 require('dotenv').config()
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.${process.env.DB_CODE}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-//mongodb+srv://Abir:<password>@cluster0.czzkl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 const app = express()
 
 app.use(bodyParser.json());
@@ -30,71 +29,21 @@ client.connect(err => {
     const opinionCollection = client.db(`${process.env.DB_FILE}`).collection("allOpinion");
     const appointmentCollection = client.db(`${process.env.DB_FILE}`).collection("allAppointment");
     const universityCollection = client.db(`${process.env.DB_FILE}`).collection("allUniversity");
+    
+    //for order
     app.post('/addOrder', (req, res) => {
         const order = req.body;
-        console.log(order);
         orderCollection.insertOne(order)
             .then(result => {
                 res.send(result.insertedCount > 0);
             })
     })
-    // app.post('/addFood', (req, res) => {
-    //     const file = req.files.file;
-    //     const image = req.files.file.name;
-    //     const title = req.body.title;
-    //     const price = req.body.price;
-    //     const category = req.body.category;
-    //     const description = req.body.description;
-    //     const shortDescription = req.body.shortDescription;
-
-    //     file.mv(`${__dirname}/food/${file.name}`,err=>{
-    //         if(err){
-    //             return res.status(500).send({msg:'Failed to upload Image'});
-    //         }
-    //     })
-
-    //     foodCollection.insertOne({ title, price, category, description, shortDescription, image })
-    //         .then(result => {
-    //             res.send(result.insertedCount > 0);
-    //         })
-    // })
-    // app.get('/foods', (req, res) => {
-    //     foodCollection.find({})
-    //         .toArray((err, documents) => {
-    //             res.send(documents);
-    //         })
-    // })
-    // app.delete('/delete/:id', (req, res) => {
-    //     console.log(req.params.id);
-    //     studentCollection.deleteOne({ _id: ObjectId(req.params.id) })
-    //         .then((result) => {
-    //             res.send(result.deletedCount > 0);
-    //             console.log(res);
-    //         })
-    // })
-
     app.get('/allOrder', (req, res) => {
         orderCollection.find({})
             .toArray((err, documents) => {
                 res.send(documents);
             })
     })
-
-    // app.get('/students/:department/:roll', (req, res) => {
-    //     studentCollection.find({ roll: req.params.roll, department: req.params.department })
-    //         .toArray((err, documents) => {
-    //             res.send(documents[0]);
-    //         })
-    // })
-
-    // app.post('/studentsByRoll', (req, res) => {
-    //     const roll = req.body;
-    //     studentCollection.find({ roll: roll.roll })
-    //         .toArray((err, documents) => {
-    //             res.send(documents);
-    //         })
-    // })
-
     app.patch('/updateOrder/:id', (req, res) => {
         orderCollection.updateOne({ _id: ObjectId(req.params.id) },
             {
@@ -106,7 +55,8 @@ client.connect(err => {
                 res.send(result.matchedCount > 0);
             })
     })
-
+    
+    //for amount
     app.patch('/updateAmount/:id', (req, res) => {
         orderCollection.updateOne({ _id: ObjectId(req.params.id) },
             {
@@ -139,16 +89,12 @@ client.connect(err => {
                 res.send(result.insertedCount > 0);
             })
     })
-
-
     app.get('/items', (req, res) => {
         itemCollection.find({})
             .toArray((err, documents) => {
                 res.send(documents);
             })
     })
-
-
     app.get('/item/:id', (req, res) => {
         itemCollection.find({ _id: ObjectId(req.params.id) })
             .toArray((err, documents) => {
@@ -170,43 +116,58 @@ client.connect(err => {
             })
     })
     app.delete('/deleteItem/:id', (req, res) => {
-        console.log(req.params.id);
         itemCollection.deleteOne({ _id: ObjectId(req.params.id) })
             .then((result) => {
                 res.send(result.deletedCount > 0);
-                console.log(res);
             })
     })
 
      //for university
      app.post('/addUniversity', (req, res) => {
         const data = req.body;
-        // console.log(req)
         universityCollection.insertOne({ data })
             .then(result => {
                 res.send(result.insertedCount > 0);
             })
     })
-
-
     app.get('/universities', (req, res) => {
         universityCollection.find({})
             .toArray((err, documents) => {
                 res.send(documents);
             })
     })
+    app.get('/university/:id', (req, res) => {
+        universityCollection.find({ _id: ObjectId(req.params.id) })
+            .toArray((err, documents) => {
+                res.send(documents[0]);
+            })
+    })
+    app.delete('/deleteUniversity/:id', (req, res) => {
+        universityCollection.deleteOne({ _id: ObjectId(req.params.id) })
+            .then((result) => {
+                res.send(result.deletedCount > 0);
+            })
+    })
+    app.patch('/updateUniversity/:id', (req, res) => {
+        universityCollection.updateOne({ _id: ObjectId(req.params.id) },
+            {
+                $set: {
+                    data: req.body
+                },
+            })
+            .then(result => {
+                res.send(result.matchedCount > 0);
+            })
+    })
 
     //for questions 
-
     app.post('/addQuestion', (req, res) => {
         const data = req.body;
-        // console.log(req)
         questionCollection.insertOne({ data })
             .then(result => {
                 res.send(result.insertedCount > 0);
             })
     })
-
     app.get('/questions', (req, res) => {
         questionCollection.find({})
             .toArray((err, documents) => {
@@ -220,11 +181,9 @@ client.connect(err => {
             })
     })
     app.delete('/deleteQuestion/:id', (req, res) => {
-        console.log(req.params.id);
         questionCollection.deleteOne({ _id: ObjectId(req.params.id) })
             .then((result) => {
                 res.send(result.deletedCount > 0);
-                console.log(res);
             })
     })
     app.patch('/updateQuestion/:id', (req, res) => {
@@ -239,24 +198,19 @@ client.connect(err => {
             })
     })
 
-
     //for teacher
     app.post('/addTeacher', (req, res) => {
-        // const data = req.body
         const category = req.body.category;
         const designation = req.body.designation;
         const status = req.body.status;
         const subject = req.body.subject;
         const teacherName = req.body.teacherName;
         const workingPlace = req.body.workingPlace;
-        // console.log(req)
         teacherCollection.insertOne({ category, designation, status, subject, teacherName, workingPlace })
             .then(result => {
                 res.send(result.insertedCount > 0);
             })
     })
-
-
     app.get('/teachers', (req, res) => {
         teacherCollection.find({})
             .toArray((err, documents) => {
@@ -270,21 +224,13 @@ client.connect(err => {
             })
     })
     app.delete('/deleteTeacher/:id', (req, res) => {
-        console.log(req.params.id);
         teacherCollection.deleteOne({ _id: ObjectId(req.params.id) })
             .then((result) => {
                 res.send(result.deletedCount > 0);
-                console.log(res);
             })
     })
     app.patch('/updateTeacher/:id', (req, res) => {
         teacherCollection.updateOne({ _id: ObjectId(req.params.id) },
-        // const category = req.body.category;
-        // const designation = req.body.designation;
-        // const status = req.body.status;
-        // const subject = req.body.subject;
-        // const teacherName = req.body.teacherName;
-        // const workingPlace = req.body.workingPlace;
             {
                 $set: {
                     category: req.body.category,
@@ -293,8 +239,6 @@ client.connect(err => {
                     subject: req.body.subject,
                     teacherName: req.body.teacherName,
                     workingPlace: req.body.workingPlace,
-                    // console.log(req)
-                    // approvedData: req.body.approvedData
                 },
             })
             .then(result => {
@@ -302,17 +246,14 @@ client.connect(err => {
             })
     })
 
-
     //for opinion
     app.post('/addOpinion', (req, res) => {
         const data = req.body;
-        // console.log(req)
         opinionCollection.insertOne({ data })
             .then(result => {
                 res.send(result.insertedCount > 0);
             })
     })
-
     app.get('/opinions', (req, res) => {
         opinionCollection.find({})
             .toArray((err, documents) => {
@@ -320,21 +261,17 @@ client.connect(err => {
             })
     })
 
-
     //for appointment
     app.post('/addAppointment', (req, res) => {
-        // const data = req.body
         const email = req.body.email;
         const teacher = req.body.teacher;
         const status = req.body.status;
-        // console.log(req)
         const approvedData = {};
         appointmentCollection.insertOne({ email, teacher, status, approvedData })
             .then(result => {
                 res.send(result.insertedCount > 0);
             })
     })
-
     app.get('/appointments', (req, res) => {
         appointmentCollection.find({})
             .toArray((err, documents) => {
@@ -348,7 +285,6 @@ client.connect(err => {
                     email: req.body.email,
                     teacher: req.body.teacher,
                     status: req.body.status,
-                    // console.log(req)
                     approvedData: req.body.approvedData
                 },
             })
